b01a41fba0d634dcdeae5884946f8e25f3887f5d
Defri Indra Mahardika <defrindr@gmail.com>
2021-08-14 15:45:46 +0700

    not-found-middleware

--- app/middleware/notFoundHandler.middleware.js
+++ app/middleware/notFoundHandler.middleware.js
@@ -1,15 +1,15 @@
-var createError = require('http-errors');
+var createError = require("http-errors");
 
 module.exports = function (req, res, next) {
-    // hadle page not found json
-    if(req.headers['content-type']){
-        if (req.headers['content-type'].toLowerCase() == "application/json") {
-            return res.status(404).json({
-                success: false,
-                message: "Page not found",
-                data: null,
-            });
-        }
+  // hadle page not found json
+  if (req.headers["content-type"]) {
+    if (req.headers["content-type"].toLowerCase() == "application/json") {
+      return res.status(404).json({
+        success: false,
+        message: "Page not found",
+        data: null,
+      });
     }
-    next(createError(404));
+  }
+  next(createError(404));
 };
