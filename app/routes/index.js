bd1621950b74328c18b5515e8faf6116938e692a
Defri Indra Mahardika <defrindr@gmail.com>
2021-08-16 15:59:24 +0700

    remove app/route and register route in config

--- app/routes/index.js
+++
@@ -1,5 +0,0 @@
-var express = require("express");
-const RegisterRoute = require("../../core/register.controller");
-var router = express.Router();
-
-module.exports = RegisterRoute(router);
--- config/route.config.js
+++ config/route.config.js
@@ -1,22 +1,25 @@
-
 var middleware = require("app/middleware/index");
-var router = require("routes/index");
+var express = require("express");
+const RegisterRoute = require("../core/register.controller");
+var router = express.Router();
 
-module.exports= function(config){
-    const { app } = config;
+module.exports = function (config) {
+  const { app } = config;
 
-    // register routes
-    app.use("/", router);
-    // catch 404 and forward to error handler
-    app.use(middleware.notFoundHandler);
+  // register routes
+  app.use("/", router);
+  RegisterRoute(router);
 
-    // error handler
-    app.use(function (err, req, res, next) {
-      // set locals, only providing error in development
-      res.locals.message = err.message;
-      res.locals.error = req.app.get("env") === "development" ? err : {};
-      // render the error page
-      res.status(err.status || 500);
-      res.render("error");
-    });
-}
+  // catch 404 and forward to error handler
+  app.use(middleware.notFoundHandler);
+
+  // error handler
+  app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+  });
+};
